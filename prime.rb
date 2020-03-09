42096feaf9380a74e7566e5157a4308ba4c6b73d
Pharia Le <pharia.le@gmail.com>
2020-03-09 23:22:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
+require 'pry'
+
 def prime?(num)
-  if num <= 1 
-    return false 
-  elsif num == 2 
-    return true
+  if num <= 1
+    return false
+  # elsif num == 2
+  #   return true
   end
- (2..num-1).to_a.none? { |x| num % x == 0 }
+
+  (2..num-1).to_a.none? { |x| num % x == 0 }
+  binding.pry
 end
